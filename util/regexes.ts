85e273a4957e1beb2a85c928938ee3fa86c3dc1c
Kayden Tebau <me@nullob.si>
2022-11-29 13:19:50 -0800

    Fix port regex, use backreferences

--- util/regexes.ts
+++ util/regexes.ts
@@ -1,4 +1,4 @@
-const extendedPort = /[\x21-\x7E](?<addrFamily>[0-9]*)[\x21-\x7E](?<host>.*)[\x21-\x7E](?<port>[0-9]*)[\x21-\x7E]/;
+const extendedPort = /\(([\x21-\x7E])(?<addrFamily>[0-9]*)\1(?<host>[0-9a-fA-F:.]*)\1(?<port>[0-9]*)\1\)/;
 const port = /([0-9]+),([0-9]+),([0-9]+),([0-9]+),([0-9]+),([0-9]+)/;
 const path = /"(.+)"/;
 const mdtmReply = /(?<year>[0-9]{4})(?<month>[0-9]{2})(?<day>[0-9]{2})(?<hour>[0-9]{2})(?<minute>[0-9]{2})(?<second>[0-9]{2})(?<ms>\.[0-9]+)?/;
